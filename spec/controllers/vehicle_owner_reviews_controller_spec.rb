040d25fdb15a87e4d29fbea9a9c2dae05342733c
thiennguyendev <thiennguyen.mark@gmail.com>
2019-05-08 13:36:11 +0700

    Fixing rubocop issue

--- spec/controllers/vehicle_owner_reviews_controller_spec.rb
+++ spec/controllers/vehicle_owner_reviews_controller_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe VehicleOwnerReviewsController, type: :controller do
-  describe "#show" do
+  describe '#show' do
     before :each do
       get :show
     end
 
-    it "has a 200 status code" do
+    it 'has a 200 status code' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "responds to json by default" do
-      expect(response.content_type).to eq "application/json"
+    it 'responds to json by default' do
+      expect(response.content_type).to eq 'application/json'
     end
   end
 end
--- spec/integration/vehicle_owner_review_spec.rb
+++ spec/integration/vehicle_owner_review_spec.rb
@@ -4,7 +4,7 @@ describe 'Vehicle Owner Review Show Items' do
   path '/vehicle_owner_reviews' do
     get 'Vehicle Owner Review List' do
       tags 'VehicleOwnerReview'
-      produces 'application/json', 'application/xml'
+      produces 'application/json'
 
       response '200', 'response successfully' do
         schema type: :object,
