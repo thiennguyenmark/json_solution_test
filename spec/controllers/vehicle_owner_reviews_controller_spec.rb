e3685ff25aab5650e2991ead480724ff112f44e0
thiennguyendev <thiennguyen.mark@gmail.com>
2019-05-08 13:08:21 +0700

    Refactor coding

--- spec/controllers/vehicle_owner_reviews_controller_spec.rb
+++ spec/controllers/vehicle_owner_reviews_controller_spec.rb
@@ -2,13 +2,16 @@ require 'rails_helper'
 
 RSpec.describe VehicleOwnerReviewsController, type: :controller do
   describe "#show" do
-    it "has a 200 status code" do
+    before :each do
       get :show
+    end
+
+    it "has a 200 status code" do
       expect(response).to have_http_status(:ok)
     end
 
     it "responds to json by default" do
-      expect(response.content_type).to eq nil
+      expect(response.content_type).to eq "application/json"
     end
   end
 end
